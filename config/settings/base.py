a9980eb095d2d17b85b16458655d752164adb50e
22210301016 <bolgamhemanth7878@gmail.com>
2026-09-17 22:08:19 +0530

    setting files are added for the development and production

--- config/settings/base.py
+++ config/settings/base.py
@@ -13,17 +13,17 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fyaf+8x@nx4sr^g!wk3ha$8@9!e8i8_sa1o-sdb1l(42m2e_!m'
+# SECRET_KEY = 'django-insecure-fyaf+8x@nx4sr^g!wk3ha$8@9!e8i8_sa1o-sdb1l(42m2e_!m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
 ALLOWED_HOSTS = []
 
---
+++ config/settings/development.py
@@ -0,0 +1,8 @@
+from .base import * 
+
+
+DEBUG=True
+SECRET_KEY='temporary-dev-key-we-will-fix-with-env-vars-next'
+ALLOWED_HOSTS=['localhost','127.0.0.1']
+
+
